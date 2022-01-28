7f242cc1f97309a6520d5264e76633d274ec2b3b
Dmitry Eremenko <wait4thesun@gmail.com>
2022-01-28 12:07:42 +0300

    Now it prints tables

--- internal/ddl/generator.go
+++ internal/ddl/generator.go
@@ -5,14 +5,14 @@ import (
 	"github.com/jmoiron/sqlx"
 )
 
-func GenerateDDL(db *sqlx.DB, schemaName string) (string, error) {
+func GenerateDDL(db *sqlx.DB, schemaName string) ([]string, error) {
 	tableNames := fmt.Sprintf(`select table_name from information_schema.tables where table_schema = '%s';`, schemaName)
 
 	tables := make([]string, 0)
 	err := db.Select(&tables, tableNames)
 	if err != nil {
-		return "", err
+		return nil, err
 	}
 
-	return "", nil
+	return tables, nil
 }
